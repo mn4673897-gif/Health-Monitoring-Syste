03a950c9821fdc22293b456b56eca09f770039d2
mn4673897-gif <mn4673897@gmail.com>
2026-04-22 23:31:30 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDnDYwW1xhuTvK3U6DqthhH468kGlF0fYU",
+    authDomain: "health-monitoring-22a98.firebaseapp.com",
+    projectId: "health-monitoring-22a98",
+    storageBucket: "health-monitoring-22a98.firebasestorage.app",
+    messagingSenderId: "617570428934",
+    appId: "1:617570428934:web:5080502340423fdbefbb87"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
